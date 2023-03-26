97240880e37b07987007f82ad2de89dd7e4ade87
sevdeutsch <severin.deutschmann@gmail.com>
2023-03-26 20:11:23 +0100

    New Buildings Added

--- js/modules/researchManager.js
+++ js/modules/researchManager.js
@@ -66,7 +66,12 @@ window.researchManager = new ResearchManager();
 
 // Add research options here
 window.researchManager.addResearch(new Research('expansionTech', 'Expansion Tech', { researchPoints: 4 }));
-window.researchManager.addResearch(new Research('steelMaking', 'Steel Making', { researchPoints: 12 }));
+window.researchManager.addResearch(new Research('remoteConstruction', 'Remote Construction', { researchPoints: 12 }));
+window.researchManager.addResearch(new Research('steelMaking', 'Steel Making', { researchPoints: 24 }));
+window.researchManager.addResearch(new Research('oilProcessing', 'Oil Processing', { researchPoints: 30 }));
+window.researchManager.addResearch(new Research('advancedElectronics', 'Advanced Electronics', { researchPoints: 36 }));
+window.researchManager.addResearch(new Research('blueprintTech', 'Blueprint Tech', { researchPoints: 40 }));
+window.researchManager.addResearch(new Research('beaconTech', 'Beacon Tech', { researchPoints: 100 }));
 window.researchManager.addResearch(new Research('gameWon', 'Win the Demo', { researchPoints: 400 }));
 window.researchManager.addResearch(new Research('gameWon2', 'Win Harder', { researchPoints: 4000 }));
 window.researchManager.addResearch(new Research('gameWon3', 'Win too Hard', { researchPoints: 40000 }));
--- js/modules/resources.js
+++ js/modules/resources.js
@@ -3,16 +3,23 @@ const resourceCategories = {
   coal: { category: "Raw", order: 2 },
   ironOre: { category: "Raw", order: 3 },
   copperOre: { category: "Raw", order: 4 },
+  oilBarrel: { category: "Raw", order: 5 },
   bricks: { category: "Basic", order: 1 },
   ironPlates: { category: "Basic", order: 2 },
   copperPlates: { category: "Basic", order: 3 },
   gears: { category: "Basic", order: 4 },
   copperCables: { category: "Basic", order: 5 },
+  steel: { category: "Basic", order: 6 },
+  petroleumBarrel: { category: "Basic", order: 7 },
+  plastics: { category: "Basic", order: 8 },
+  sulfur: { category: "Basic", order: 9 },
   greenChips: { category: "Advanced", order: 1 },
-  redScience: { category: "Advanced", order: 2 },
+  redChips: { category: "Advanced", order: 2 },
+  redScience: { category: "Advanced", order: 3 },
   researchPoints: { category: "Points", order: 1 },
   expansionPoints: { category: "Points", order: 2 },
-  steel: { category: "Other", order: 1 },
+  blueprintPoints: { category: "Points", order: 3 },
+
 };
 
 window.resourceCategories = Object.keys(resourceCategories)
